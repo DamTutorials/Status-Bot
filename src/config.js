79e7a6f26a89c0d26e078e5b36a3de20f5f46e02
DamTutorials <94178969+DamTutorials@users.noreply.github.com>
2023-09-22 20:54:40 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -17,16 +17,16 @@ export default {
 	interval: ms('1 minute'), //e.g 5 minute, 2 minute, 30 second
 	embeds: {
 		downtime: {
-			title: 'Server is down!', //keep the title short
-			content: '<@&ROLE_ID>', // replace ROLE_ID with your ping role id
+			title: 'Bot is down!', //keep the title short
+			content: '<@1057001259514744852>', // replace ROLE_ID with your ping role id
 			description: 'Server {server_name} is down! We are Working to fix this!', //change to whatever you want it to be
 			color: '#FF0000' //red by default
 		},
 		uptime: {
-			title: 'Server is back!', //keep the title short
-			content: '<@&ROLE_ID>', // replace ROLE_ID with your ping role id
+			title: 'Bot is back!', //keep the title short
+			content: '<@1057001259514744852>', // replace ROLE_ID with your ping role id
 			// Note: anything within "{}" is a dynamic value assigned by the code
-			description: 'The server {server_name} was down for {time} of time',
+			description: 'The bot {server_name} was down for {time} of time',
 			color: '#008000' //green by default
 		}
 	}
